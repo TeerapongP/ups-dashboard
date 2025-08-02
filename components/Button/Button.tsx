6f8db02fbd92fc8bf555ed72fded38d161196a40
TeerapongP <thirapongp7@gmail.com>
2025-08-02 09:32:49 +0700

    feat: update Button component to accept label prop

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = () => {
+const Button = (props: { label: any }) => {
     return (
-        <button>Button</button>
+        <button>{props.label}</button>
     )
 }
 
