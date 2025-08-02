e21a02fde9159a85ad2aefaab5ef8874c314a858
TeerapongP <thirapongp7@gmail.com>
2025-08-02 09:42:37 +0700

    feat: update Button component to accept onClick prop for enhanced interactivity

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = (props: { label: any }) => {
+const Button = (props: { label: any, onClick: any }) => {
     return (
-        <button className='bg-blue-500 text-white p-2 rounded-md'>{props.label}</button>
+        <button className='bg-blue-500 text-white p-2 rounded-md' onClick={props.onClick}>{props.label}</button>
     )
 }
 
