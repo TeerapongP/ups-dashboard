36d03fe2decd47bb419b1e0b4cde8f56009842a4
TeerapongP <thirapongp7@gmail.com>
2025-08-02 10:09:52 +0700

    fix: add aria-label attribute to button element for improved accessibility
    - Added the 'aria-label' attribute to the button element in the Button component to enhance accessibility for screen readers, ensuring that the button's purpose is clear to users with disabilities.

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = (props: { label: any, onClick: any, disabled: boolean }) => {
     return (
-        <button className='bg-blue-500 text-white p-2 rounded-md' onClick={props.onClick} disabled={props.disabled} type='button'>{props.label}</button>
+        <button className='bg-blue-500 text-white p-2 rounded-md' onClick={props.onClick} disabled={props.disabled} type='button' aria-label={props.label}>{props.label}</button>
     )
 }
 
